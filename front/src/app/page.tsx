8a21fbf73dcf97f893e60ccf003a51fe1545f189
NNicolas Scilipoti <nicoscili@gmail.com>
2025-11-05 18:58:51 -0300

    carrousels

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,7 +1,21 @@
 /* eslint-disable @next/next/no-img-element */
+import HorizontalScroller from "../components/HorizontalScroller";
+import ArtistsPreviewCard from "../components/artistsPreviewCard";
+import IUser from "../interfaces/IUser";
+import { userMockData } from "../mocks/UserMock";
+import { IEvent } from "@/src/interfaces/IEvent";
+import { mockData } from "@/src/mocks/EventMock";
+import { bandMockData } from "@/src/mocks/BandMock";
+import { IBand } from "@/src/interfaces/IBand";
+import BandsPreviewCard from "@/src/components/BandsPreviewCard";
+import EventPreviewCard from "@/src/components/eventPreviewCard";
 import Link from "next/link";
 
 export default function LandingPage() {
+  const artists: IUser[] = userMockData;
+  const bands: IBand[] = bandMockData;
+  const quotes: IEvent[] = mockData;
+
   return (
     <main>
       <div className="flex flex-col justify-center text-center w-[70%] mx-auto my-24 gap-1.5">
@@ -11,22 +25,8 @@ export default function LandingPage() {
         <p className="text-txt2 mx-auto w-[50%] text-md">
           Conecta con otros artistas, descubre proyectos y oportunidades para colaborar en tu próxima canción.
         </p>
-        <div className="flex flex-row gap-2 justify-around mx-auto mt-8 w-[40%]">
-          <Link
-          className="bg-tur1 py-1.5 px-4 rounded-md text-azul text-lg font-sans shadow-xl transition duration-300  hover:bg-tur2 hover:text-oscuro2 hover:-translate-y-0.5 hover:cursor-pointer"
-          href={"/vacancyPreview"}
-          >
-          Soy artista
-          </Link>
-          <Link
-          className="py-1.5 rounded-md px-4 text-tur3 text-lg font-sans border-fondo1 transition duration-400 hover:bg-tur3 hover:border-verde hover:text-azul hover:-translate-y-0.5 hover:cursor-pointer"
-          href={"/artistsPreview"}
-          >
-          Busco artista
-          </Link>
-        </div>
         <img 
-          className="w-lg mx-auto my-12"
+          className="w-lg mx-auto my-8"
           src="https://ohmagazinerd.com/wp-content/uploads/2020/08/47601C44-9EF0-452C-8D19-90B9E91C7297-2.png"
           alt="img"
         />
@@ -65,7 +65,7 @@ export default function LandingPage() {
             </p>
           </div>
         </div>
-        <div className="flex flex-row justify-around w-[80%] mx-auto mt-16">
+        <div className="flex flex-row justify-around w-[80%] mx-auto mt-12">
           <div className="flex flex-col">
             <h2 className="text- text-2xl font-bold">
               ¿Deseas ver más?
@@ -88,6 +88,63 @@ export default function LandingPage() {
               ↪ Registrarse 
             </Link>
           </div>
+        </div>
+        <div className="mt-16">
+          <h1 className="text-txt1 font-bold text-center text-3xl text-shadow-md mb-4">Nuestros preciados artistas</h1>
+          <div className="mx-auto max-w-[83%] rounded-4xl py-1.5 px-2">
+            <HorizontalScroller>
+              {artists.map((artist) => (
+                <Link
+                  href={`/home`}
+                  key={artist.id}
+                  className="shrink-0 w-64 flex justify-center snap-start"
+                >
+                  <ArtistsPreviewCard
+                    userName={artist.userName}
+                    profilePicture={artist.profilePicture}
+                    country={artist.country}
+                    averageRating={artist.averageRating}
+                  />
+                </Link>
+              ))}
+            </HorizontalScroller>
+          </div>
+        </div>
+        <h1 className="text-txt1 mt-16 font-bold text-center text-3xl text-shadow-md mb-4 ">Algunas de nuestras bandas registradas...</h1>
+        <div className="mx-auto max-w-[83%] rounded-4xl py-1.5 px-2">
+          <HorizontalScroller>
+            {bands.map((band) => (
+              <Link
+                href={`/home`}
+                key={band.id}
+                className="shrink-0 w-64 flex justify-center snap-start"
+              >
+                <BandsPreviewCard
+                  name={band.name}
+                  image={band.bandImage}
+                />
+              </Link>
+            ))}
+          </HorizontalScroller>
+        </div>
+        <h1 className="text-txt1 font-bold text-center text-3xl text-shadow-md mb-4 mt-16">¡Participa en eventos creados por las mismas bandas!</h1>
+        <div className="mx-auto max-w-[83%] rounded-4xl py-1.5 px-2">
+          <HorizontalScroller>
+            {quotes.map((quote) => (
+              <Link
+                href={`/home`}
+                key={quote.id}
+                className="shrink-0 w-64 flex justify-center snap-start"
+              >
+                <EventPreviewCard
+                  name={quote.name} 
+                  event={quote.event}
+                  image={quote.image} 
+                  date={quote.date}
+                />
+              </Link>
+            ))}
+          </HorizontalScroller>
         </div>
       </div>
     </main>
