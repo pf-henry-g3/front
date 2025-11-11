bc45723d3dd3051917b9b71608ce6a4b5adedb81
nachooaguirre <ignacioaguirre861@gmail.com>
2025-11-10 23:20:23 -0300

    fix(build): add IEvent interface used by EventMock

---
+++ front/src/interfaces/IEvent.ts
@@ -0,0 +1,12 @@
+export interface IEvent {
+  id: number;
+  name: string;
+  category: string;
+  event: string;
+  date: Date;
+  location: string;
+  description: string;
+  image: string;
+}
+
+
