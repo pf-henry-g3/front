30430ff7f211b8860500fabbc91ba58a703b3b04
fernando <ferna.arancibia.c@gmail.com>
2025-11-18 00:58:20 +0100

    conflict resolve loginform

--- front/src/interfaces/IAuthContextType.ts
+++ front/src/interfaces/IAuthContextType.ts
@@ -2,10 +2,9 @@ import IUser from "./IUser"
 
 export default interface AuthContextType {
     user: IUser | null;
-    token: string | null;
     loading: boolean;
     isAuthenticated: boolean;
-    login: (user: IUser, token: string) => void;
+    login: (user: IUser) => void;
     logout: () => Promise<void>;
     refreshUser: () => Promise<void>;
     getToken?: () => string | null;
